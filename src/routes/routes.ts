9075a6c4e04f7ed626b08164c8d27b317bbb46dc
Rafael Fávaro Azzi <rafaelfavazzi@gmail.com>
2020-06-30 15:58:17 -0300

    Removing response

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,12 +6,8 @@ import UserController from "../controllers/userController"
 import SessionController from "../controllers/sessionController"
 import { Test } from "../controllers/test"
 
-
-import { Responses } from "../middlewares/responses"
 // Middleware
 import Auth from "../middlewares/auth"
-
-routes.use(Responses)
 
 // Rotas sem autenticação
 routes.get("/users", UserController.index)
