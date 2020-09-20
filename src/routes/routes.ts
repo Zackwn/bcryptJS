e506e39a0ecb8ccfa974c10a11306ff5c9fa5191
Zackwn <rafaelfavazzi@gmail.com>
2020-09-20 20:24:11 -0300

    update

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -9,15 +9,17 @@ import { Test } from "../controllers/test"
 // Middleware
 import Auth from "../middlewares/auth"
 
-// Rotas sem autenticação
+// Rotas sem autenticação 
+routes.get("/", (_,res) => res.send("Ok"))
+
 routes.get("/users", UserController.index)
 routes.post("/create/user", UserController.create)
 routes.post("/login", SessionController.login)
 routes.put("/users/:id/edit", UserController.update)
 
-routes.use(Auth)
-
+// routes.use(Auth)
+ 
 // Rotas autenticadas
-routes.get("/", Test)
+routes.get("/test", Test)
 
 export default routes
